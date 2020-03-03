813986a5dc54ee3a26d2ff3859d8067eaf3d0a5f
mywinds1015 <61447551+mywinds1015@users.noreply.github.com>
2020-03-03 17:24:25 +0800

    新增

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FrontController extends Controller
+{
+    public function index(){
+        return view('front/index');
+    }
+    public function news(){
+        $news_data=DB::table('news')->get();
+        return view('front/news',compact('news_data'));
+    }
+
+}
